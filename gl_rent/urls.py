d56c603929db838394416996b9105be9235b2ebd
Igor Popov <ihptru@gmail.com>
2016-01-12 11:09:51 +0000

    REST serializers and views

--- gl_rent/urls.py
+++ gl_rent/urls.py
@@ -1,5 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from rest_framework import routers
+from gl_rent import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-	
+	url(r'^rest/', include(router.urls)),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
