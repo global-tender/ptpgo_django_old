2e6ebb1778eeee0eb1a74221092d3f3947cae270
Igor Popov <ihptru@gmail.com>
2016-04-07 11:57:13 +0000

    update admin.py

--- ptpgo/admin.py
+++ ptpgo/admin.py
@@ -6,8 +6,20 @@ from ptpgo.models import ClientRatings
 from ptpgo.models import ClientSiteNotifications
 from ptpgo.models import ClientNotificationsLog
 
+from ptpgo.models import ListBoat
+from ptpgo.models import ListBoatPhotos
+
+from ptpgo.models import OrderBoat
+from ptpgo.models import Reviews
+
 admin.site.register(Clients)
 admin.site.register(ClientPhotos)
 admin.site.register(ClientRatings)
 admin.site.register(ClientSiteNotifications)
 admin.site.register(ClientNotificationsLog)
+
+admin.site.register(ListBoat)
+admin.site.register(ListBoatPhotos)
+
+admin.site.register(OrderBoat)
+admin.site.register(Reviews)
